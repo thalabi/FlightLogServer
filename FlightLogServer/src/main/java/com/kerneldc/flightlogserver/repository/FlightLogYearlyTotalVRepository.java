e01fe56c6787131c824118e4879353b07b1d8020
Tarif Halabi <tarif.halabi@gmail.com>
2018-03-13 22:46:20 -0400

    Add FlightLogYearlyTotal view.

---
+++ FlightLogServer/src/main/java/com/kerneldc/flightlogserver/repository/FlightLogYearlyTotalVRepository.java
@@ -0,0 +1,16 @@
+package com.kerneldc.flightlogserver.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.kerneldc.flightlogserver.domain.FlightLogYearlyTotalV;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+public interface FlightLogYearlyTotalVRepository extends JpaRepository<FlightLogYearlyTotalV, Long>, JpaSpecificationExecutor<FlightLogYearlyTotalV> {
+	List<FlightLogYearlyTotalV> findAllByOrderById();
+}
