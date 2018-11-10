fa50778773e3064ea417640725a0d0f38509218a
Tarif Halabi <tarif.halabi@gmail.com>
2018-11-10 13:41:39 -0500

    Fix case when last_number of sequence is 1.

--- FlightLogServer/src/main/java/com/kerneldc/flightlogserver/batch/util/DatabaseUtil.java
+++ FlightLogServer/src/main/java/com/kerneldc/flightlogserver/batch/util/DatabaseUtil.java
@@ -46,6 +46,9 @@ public class DatabaseUtil {
 			List<Integer> lastNumberList = new JdbcTemplate(dataSource).queryForList(
 					String.format("select last_number from all_sequences where sequence_owner = user and lower(sequence_name) = '%s'", sequenceName),
 					Integer.class);
+			if (lastNumberList.get(0).compareTo(Integer.valueOf(1)) == 0) {
+				return;
+			}
 			new JdbcTemplate(dataSource).execute(String.format("alter sequence %s increment by %d nocache nominvalue", sequenceName, -1*(lastNumberList.get(0)-1)));
 			new JdbcTemplate(dataSource).execute(String.format("select %s.nextval from dual", sequenceName));
 			new JdbcTemplate(dataSource).execute(String.format("alter sequence %s increment by 1", sequenceName));
