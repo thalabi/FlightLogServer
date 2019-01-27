6ce4ccc16c8ee3474aa4b74796efccc29fd43286
Tarif Halabi <tarif.halabi@gmail.com>
2019-01-27 12:43:27 -0500

    Refactor.

--- FlightLogServer/src/main/java/com/kerneldc/flightlogserver/batch/util/ReplicationUtil.java
+++ FlightLogServer/src/main/java/com/kerneldc/flightlogserver/batch/util/ReplicationUtil.java
@@ -23,11 +23,11 @@ public class ReplicationUtil {
 		}
 	}
 	
-	public static void enableTriggers(DataSource dataSource, String tableName) throws DataAccessException, SQLException {
+	public static void enableTriggers(DataSource dataSource, String tableName) throws SQLException {
 		alterTriggers(dataSource, tableName, true);
 	}
 	
-	public static void disableTriggers(DataSource dataSource, String tableName) throws DataAccessException, SQLException {
+	public static void disableTriggers(DataSource dataSource, String tableName) throws SQLException {
 		alterTriggers(dataSource, tableName, false);
 	}
 	
@@ -41,13 +41,12 @@ public class ReplicationUtil {
 	 */
 	private static void alterTriggers(DataSource dataSource, String tableName, boolean enable) throws SQLException {
 		if (isOracleDatabase(dataSource)) {
-			System.out.println("alterTriggers: "+enable);
 			new JdbcTemplate(dataSource).execute(String.format("call common.trigger_status_pkg.set_trigger_status('%s',%d)", tableName, enable ? 1 : 0));
 		}
 	}
 	
 	public static void resetSequence(DataSource dataSource, String tableName) throws SQLException {
-		String sequenceName = String.format("%s_seq", tableName );;
+		String sequenceName = String.format("%s_seq", tableName );
 		if (isOracleDatabase(dataSource)) {
 			List<Integer> lastNumberList = new JdbcTemplate(dataSource).queryForList(
 					String.format("select last_number from all_sequences where sequence_owner = user and lower(sequence_name) = '%s'", sequenceName),
@@ -72,9 +71,7 @@ public class ReplicationUtil {
 		SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(dataSource)
 				.withSchemaName("common").withCatalogName("trigger_status_pkg").withFunctionName("get_trigger_status");
 		BigDecimal legacyTableTriggerStatus = simpleJdbcCall.executeFunction(BigDecimal.class, legacySchemaName, tableName);
-		System.out.println(legacyTableTriggerStatus);
 		BigDecimal outputTableTriggerStatus = simpleJdbcCall.executeFunction(BigDecimal.class, outputSchemaName, tableName);
-		System.out.println(outputTableTriggerStatus);
 		return legacyTableTriggerStatus.add(outputTableTriggerStatus).intValue();
 	}
 }
