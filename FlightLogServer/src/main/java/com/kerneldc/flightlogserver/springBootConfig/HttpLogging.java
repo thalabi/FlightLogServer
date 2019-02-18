6367b1b30b5524ae34facc109ef33175719beaec
Tarif Halabi <tarif.halabi@gmail.com>
2019-02-17 21:47:10 -0500

    Refactor

--- FlightLogServer/src/main/java/com/kerneldc/flightlogserver/springBootConfig/HttpLogging.java
+++ FlightLogServer/src/main/java/com/kerneldc/flightlogserver/springBootConfig/HttpLogging.java
@@ -1,9 +1,5 @@
 package com.kerneldc.flightlogserver.springBootConfig;
 
-import java.lang.invoke.MethodHandles;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
@@ -11,18 +7,15 @@ import org.springframework.web.filter.CommonsRequestLoggingFilter;
 @Configuration
 public class HttpLogging {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
 	@Bean
 	public CommonsRequestLoggingFilter logFilter() {
-		LOGGER.info("begin {} ...", "logFilter");
 		CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
 		filter.setIncludeQueryString(true);
 		filter.setIncludePayload(true);
 		filter.setMaxPayloadLength(10000);
 		filter.setIncludeHeaders(true);
-		filter.setAfterMessagePrefix("REQUEST DATA : ");
-		LOGGER.info("end {} ...", "logFilter");
+		filter.setBeforeMessagePrefix("HTTP Message before processing - ");
+		filter.setAfterMessagePrefix("HTTP Message after processing - ");
 		return filter;
 	}
 
