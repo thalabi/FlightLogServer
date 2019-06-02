d248b0d223272d1c187aff59c98ff8878a67708e
Tarif Halabi <tarif.halabi@gmail.com>
2019-06-02 17:29:59 -0400

    Listener to initialize and clear parts cache before and after componentItemProcessor.

---
+++ FlightLogServer/src/main/java/com/kerneldc/flightlogserver/batch/copyAircraftMaintenanceTables/ComponentItemListener.java
@@ -0,0 +1,33 @@
+package com.kerneldc.flightlogserver.batch.copyAircraftMaintenanceTables;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.listener.StepExecutionListenerSupport;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class ComponentItemListener extends StepExecutionListenerSupport {
+
+	private PartCachingService partCachingService;
+
+	public ComponentItemListener(PartCachingService partCachingService) {
+		this.partCachingService = partCachingService;
+	}
+	
+	@Override
+	public void beforeStep(StepExecution stepExecution) {
+		LOGGER.info("Initializing parts cache");
+		partCachingService.getNameToPartIdMap();
+	}
+
+	@Override
+	public ExitStatus afterStep(StepExecution stepExecution) {
+		LOGGER.info("Evicting parts cache");
+		partCachingService.clearCache();
+		return null;
+	}
+
+}
