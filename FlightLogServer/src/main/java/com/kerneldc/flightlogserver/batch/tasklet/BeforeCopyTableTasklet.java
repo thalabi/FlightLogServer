bd84f71f70a1977b849e8ab6bd6d23d4de40be04
Tarif Halabi <tarif.halabi@gmail.com>
2019-06-02 17:48:50 -0400

    Truncate table if using Oracle db otherwise delete from table.

--- FlightLogServer/src/main/java/com/kerneldc/flightlogserver/batch/tasklet/BeforeCopyTableTasklet.java
+++ FlightLogServer/src/main/java/com/kerneldc/flightlogserver/batch/tasklet/BeforeCopyTableTasklet.java
@@ -2,7 +2,6 @@ package com.kerneldc.flightlogserver.batch.tasklet;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -11,6 +10,13 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.kerneldc.flightlogserver.batch.util.ReplicationUtil;
 
+/**
+ * Task that performs the following on a table:
+ * 1. truncates table
+ * 2. resets sequence
+ * 3. disable table triggers
+ *
+ */
 public class BeforeCopyTableTasklet implements Tasklet {
 
 	private DataSource outputDataSource;
@@ -23,8 +29,13 @@ public class BeforeCopyTableTasklet implements Tasklet {
 	
 	@Override
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-		String storageClause = ReplicationUtil.isOracleDatabase(outputDataSource) ? "reuse storage" : StringUtils.EMPTY;
-		new JdbcTemplate(outputDataSource).execute(String.format("truncate table %s %s", tableName, storageClause));
+		String sql;
+		if (ReplicationUtil.isOracleDatabase(outputDataSource)) {
+			sql = String.format("truncate table %s reuse storage", tableName);
+		} else {
+			sql = String.format("delete from %s", tableName);
+		}
+		new JdbcTemplate(outputDataSource).execute(sql);
 		ReplicationUtil.resetSequence(outputDataSource, tableName);
 		ReplicationUtil.disableTriggers(outputDataSource, tableName);
 		return RepeatStatus.FINISHED;
