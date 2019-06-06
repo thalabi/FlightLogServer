8c85173a5180e87bc00e390e1206e9b37a52da48
Tarif Halabi <tarif.halabi@gmail.com>
2019-06-05 20:31:50 -0400

    Add start and end time.

--- FlightLogServer/src/main/java/com/kerneldc/flightlogserver/batch/bean/StepExecutionBean.java
+++ FlightLogServer/src/main/java/com/kerneldc/flightlogserver/batch/bean/StepExecutionBean.java
@@ -1,33 +1,23 @@
 package com.kerneldc.flightlogserver.batch.bean;
 
+import java.util.Date;
 import java.util.List;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
+@Builder
 public class StepExecutionBean {
 
-	private String jobName;
-	private String jobExitStatus;
+	private String stepName;
+	private String stepExitStatus;
 	private Integer readCount;
 	private Integer readSkipCount;
 	private Integer writeCount;
 	private Integer writeSkipCount;
+	private Date startTime;
+	private Date endTime;
 	private List<String> failureExceptionList;
-	
-	public StepExecutionBean() {
-	}
-	
-	public StepExecutionBean(String jobName, String jobExitStatus, Integer readCount, Integer readSkipCount,
-			Integer writeCount, Integer writeSkipCount, List<String> failureExceptionList) {
-		this.jobName = jobName;
-		this.jobExitStatus = jobExitStatus;
-		this.readCount = readCount;
-		this.readSkipCount = readSkipCount;
-		this.writeCount = writeCount;
-		this.writeSkipCount = writeSkipCount;
-		this.failureExceptionList = failureExceptionList;
-	}
-
 }
