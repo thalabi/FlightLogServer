c7c66a2d3ac9564375976db09b45cfc51c5a43da
Tarif Halabi <tarif.halabi@gmail.com>
2019-07-27 15:40:13 -0400

    Add ApplicationPidFileWriter listener.

--- FlightLogServer/src/main/java/com/kerneldc/flightlogserver/FlightLogServerApplication.java
+++ FlightLogServer/src/main/java/com/kerneldc/flightlogserver/FlightLogServerApplication.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 
@@ -21,14 +22,14 @@ public class FlightLogServerApplication {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-	private static ApplicationContext applicationContext;
-	
 	public static void main(String[] args) {
-		applicationContext = SpringApplication.run(FlightLogServerApplication.class, args);
-		displayAllBeans();
+		SpringApplication springApplication = new SpringApplication(FlightLogServerApplication.class);
+		springApplication.addListeners(new ApplicationPidFileWriter()); // creates a pid file
+		ApplicationContext applicationContext = springApplication.run(args);
+		displayAllBeans(applicationContext);
 	}
 
-    public static void displayAllBeans() {
+    public static void displayAllBeans(ApplicationContext applicationContext) {
         String[] beanNameList = applicationContext.getBeanDefinitionNames();
         List<String> classNameAndBeanNameList = new ArrayList<>(beanNameList.length);
         for (String beanName : beanNameList) {
