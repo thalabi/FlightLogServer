7372f812db9b456ddc6077d32deb43ebfa4cc398
Tarif Halabi <tarif.halabi@gmail.com>
2019-08-25 08:18:17 -0400

    Change from integration test to mvc test.

--- FlightLogServer/src/test/java/com/kerneldc/flightlogserver/controller/AppInfoControllerTests.java
+++ FlightLogServer/src/test/java/com/kerneldc/flightlogserver/controller/AppInfoControllerTests.java
@@ -4,30 +4,31 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import java.lang.invoke.MethodHandles;
 import java.nio.charset.Charset;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.kerneldc.flightlogserver.AbstractBaseTest;
-import com.kerneldc.flightlogserver.FlightLogServerApplication;
+import com.kerneldc.flightlogserver.security.config.UnauthorizedHandler;
+import com.kerneldc.flightlogserver.security.service.CustomUserDetailsService;
+import com.kerneldc.flightlogserver.security.util.JwtTokenProvider;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = FlightLogServerApplication.class)
+@WebMvcTest(controllers = AppInfoController.class)
 
 public class AppInfoControllerTests extends AbstractBaseTest {
 
@@ -35,22 +36,23 @@ public class AppInfoControllerTests extends AbstractBaseTest {
 
 	private static final String BASE_URI = "/appInfoController";
 	
+	@Autowired
 	private MockMvc mockMvc;
 	
-	@Autowired
-    private WebApplicationContext webApplicationContext;
+	@MockBean
+	private CustomUserDetailsService customUserDetailsService;
+	@MockBean
+	private UnauthorizedHandler unauthorizedHandler;
+	@MockBean
+	private JwtTokenProvider jwtTokenProvider;
 
 	@Value("${build.timestamp}")
 	private String buildTimestamp;
 
 	private MediaType contentType = new MediaType(MediaType.TEXT_PLAIN.getType(), MediaType.TEXT_PLAIN.getSubtype(), Charset.forName("utf8"));
 
-	@Before
-	public void setup() throws Exception {
-		mockMvc = webAppContextSetup(webApplicationContext).build();
-	}
-
 	@Test
+	@WithMockUser
 	public void testGetBuildTimestamp( ) throws Exception {
 		MvcResult mvcResult = mockMvc.perform(get(BASE_URI + "/getBuildTimestamp"))
 			.andExpect(status().isOk())
