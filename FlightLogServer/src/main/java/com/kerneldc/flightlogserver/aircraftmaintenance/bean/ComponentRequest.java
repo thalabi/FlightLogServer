bba532b747b5d71f206a4de651703162905b1511
Tarif Halabi <tarif.halabi@gmail.com>
2019-10-30 22:14:41 -0400

    Changed ComponentRequest.

--- FlightLogServer/src/main/java/com/kerneldc/flightlogserver/aircraftmaintenance/bean/ComponentRequest.java
+++ FlightLogServer/src/main/java/com/kerneldc/flightlogserver/aircraftmaintenance/bean/ComponentRequest.java
@@ -1,6 +1,8 @@
 package com.kerneldc.flightlogserver.aircraftmaintenance.bean;
 
 import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -8,9 +10,10 @@ import lombok.ToString;
 
 @Getter @Setter
 @ToString
-public class ComponentRequest {
+public class ComponentRequestNew {
 
 	private String componentUri;
+
     private String name;
     private String description;
     private String workPerformed;
@@ -18,11 +21,31 @@ public class ComponentRequest {
     private Float hoursPerformed;
     private Date dateDue;
     private Float hoursDue;
-    
+
     private String partUri;
-    
-    private Boolean createHistoryRecord;
-    
-	private Date created;
+
+    private Date created;
 	private Date modified;
+
+	private Set<ComponentHistoryVo> componentHistoryRequestSet = new LinkedHashSet<>(); 
+
+    @Getter @Setter
+    @ToString
+	class ComponentHistoryVo {
+    	
+    	private String componentHistoryUri;
+    	
+	    private String name;
+	    private String description;
+	    private String workPerformed;
+	    private Date datePerformed;
+	    private Float hoursPerformed;
+	    private Date dateDue;
+	    private Float hoursDue;
+
+	    private String partUri;
+
+	    private Date created;
+		private Date modified;
+	}
 }
