c01ccabc81b0aee629ddf76bc511dbdf089dee7f
Tarif Halabi <tarif.halabi@gmail.com>
2019-10-30 22:16:34 -0400

    Changed.

--- FlightLogServer/src/main/java/com/kerneldc/flightlogserver/aircraftmaintenance/bean/ComponentRequest.java
+++ FlightLogServer/src/main/java/com/kerneldc/flightlogserver/aircraftmaintenance/bean/ComponentRequest.java
@@ -10,7 +10,7 @@ import lombok.ToString;
 
 @Getter @Setter
 @ToString
-public class ComponentRequestNew {
+public class ComponentRequest {
 
 	private String componentUri;
 
@@ -27,25 +27,5 @@ public class ComponentRequestNew {
     private Date created;
 	private Date modified;
 
-	private Set<ComponentHistoryVo> componentHistoryRequestSet = new LinkedHashSet<>(); 
-
-    @Getter @Setter
-    @ToString
-	class ComponentHistoryVo {
-    	
-    	private String componentHistoryUri;
-    	
-	    private String name;
-	    private String description;
-	    private String workPerformed;
-	    private Date datePerformed;
-	    private Float hoursPerformed;
-	    private Date dateDue;
-	    private Float hoursDue;
-
-	    private String partUri;
-
-	    private Date created;
-		private Date modified;
-	}
+	private Set<ComponentHistoryVo> historyRequestSet = new LinkedHashSet<>(); 
 }
