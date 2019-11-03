baeff226872456ddbf371bb247af56384f6f8e46
Tarif Halabi <tarif.halabi@gmail.com>
2019-11-03 12:57:39 -0500

    Add more tests.

--- FlightLogServer/src/test/java/com/kerneldc/flightlogserver/aircraftmaintenance/service/ComponentPersistenceServiceTest.java
+++ FlightLogServer/src/test/java/com/kerneldc/flightlogserver/aircraftmaintenance/service/ComponentPersistenceServiceTest.java
@@ -2,7 +2,6 @@ package com.kerneldc.flightlogserver.aircraftmaintenance.service;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -11,13 +10,15 @@ import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.BeanUtils;
 
+import com.kerneldc.flightlogserver.aircraftmaintenance.bean.ComponentHistoryVo;
 import com.kerneldc.flightlogserver.aircraftmaintenance.bean.ComponentRequest;
 import com.kerneldc.flightlogserver.aircraftmaintenance.domain.component.Component;
+import com.kerneldc.flightlogserver.aircraftmaintenance.domain.componenthistory.ComponentHistory;
 import com.kerneldc.flightlogserver.aircraftmaintenance.domain.part.Part;
 import com.kerneldc.flightlogserver.aircraftmaintenance.repository.ComponentRepository;
 import com.kerneldc.flightlogserver.aircraftmaintenance.repository.PartRepository;
@@ -58,40 +59,147 @@ public class ComponentPersistenceServiceTest {
 		componentPersistenceService.parseAndFindComponent(componentUri);
 	}
 	
-	//@Ignore
 	@Test
-	public void testupdateComponentAndHistory_ModifyComponentWithNoHistory_Success() throws ApplicationException {
-		ComponentRequest componentRequest = new ComponentRequest();
-		componentRequest.setComponentUri("http://localhost:6001/components/7");
-		componentRequest.setName("OilFilter");
-		componentRequest.setDescription("Champion CH48110");
-		componentRequest.setWorkPerformed("replaced");
-		componentRequest.setDatePerformed(new Date());
-		componentRequest.setHoursPerformed(1000f);
-		componentRequest.setDateDue(new Date());
-		componentRequest.setHoursDue(1500f);
-		componentRequest.setPartUri("http://localhost:6001/parts/258");
+	public void testUpdateComponentAndHistory_ModifyComponentWithNoHistory_Success() throws ApplicationException {
+		ComponentRequest componentRequest = ComponentRequest.builder()
+				.componentUri("http://localhost:6001/components/7").name("OilFilter").description("Champion CH48110")
+				.workPerformed("replaced").datePerformed(new Date()).hoursPerformed(1000f).dateDue(new Date())
+				.hoursDue(1500f).partUri("http://localhost:6001/parts/258").build();
 		
 		Part oldPart = Part.builder().id(25l).build();
 		Part newPart = Part.builder().id(258l).name("part 258 name").build();
 		Component oldComponent = Component.builder().id(7l).part(oldPart).name("old name").build();
-		Component newComponent = new Component();
-		BeanUtils.copyProperties(componentRequest, newComponent);
-		newComponent.setId(7l);
-		newComponent.setPart(newPart);
 
-		//when(componentPersistenceService.parseAndFindPart(componentRequest.getPartUri())).thenReturn(newPart);
-		
 		Optional<Component> optionalComponent = Optional.of(oldComponent);
 		when(componentRepository.findById(7l)).thenReturn(optionalComponent);
 		Optional<Part> optionalPart = Optional.of(newPart);
 		when(partRepository.findById(258l)).thenReturn(optionalPart);
 		
-		//when(componentPersistenceService.parseAndFindComponent(componentRequest.getComponentUri())).thenReturn(oldComponent);
-
 		componentPersistenceService.updateComponentAndHistory(componentRequest);
 		
-		verify(componentRepository).save(argThat(component -> component.getId().equals(7l) && component.getName().equals("OilFilter")));
+		ArgumentCaptor<Component> componentArg = ArgumentCaptor.forClass(Component.class);
+		verify(componentRepository).save(componentArg.capture());
+		assertThat(componentArg.getValue().getName(), equalTo(componentRequest.getName()));
+		assertThat(componentArg.getValue().getDescription(), equalTo(componentRequest.getDescription()));
+		assertThat(componentArg.getValue().getWorkPerformed(), equalTo(componentRequest.getWorkPerformed()));
+		assertThat(componentArg.getValue().getHoursPerformed(), equalTo(componentRequest.getHoursPerformed()));
+		assertThat(componentArg.getValue().getComponentHistorySet().size(), equalTo(0));
+	}
+
+	@Test
+	public void testUpdateComponentAndHistory_ModifyComponentAndAddHistoryRecord_Success() throws ApplicationException {
+		// Set up request component
+		ComponentRequest componentRequest = ComponentRequest.builder()
+				.componentUri("http://localhost:6001/components/7").name("OilFilter").description("Champion CH48110")
+				.workPerformed("replaced").datePerformed(new Date()).hoursPerformed(1000f).dateDue(new Date())
+				.hoursDue(1500f).partUri("http://localhost:6001/parts/258").build();
+		// Set up request history record
+		ComponentHistoryVo componentHistoryVo = ComponentHistoryVo.builder().name("OilFilter")
+				.description("Tempest CH48110").workPerformed("replaced").datePerformed(new Date())
+				.hoursPerformed(1000f).partUri("http://localhost:6001/parts/259").build();
+		componentRequest.getHistoryRequestSet().add(componentHistoryVo);
+		
+		Part oldPart = Part.builder().id(25l).build();
+		Part newPart = Part.builder().id(258l).name("part 258 name").build();
+		Part newHistoryPart = Part.builder().id(259l).name("part 259 name").build();
+		Component oldComponent = Component.builder().id(7l).part(oldPart).name("old name").build();
+
+		Optional<Component> optionalComponent = Optional.of(oldComponent);
+		when(componentRepository.findById(7l)).thenReturn(optionalComponent);
+		Optional<Part> optionalPart = Optional.of(newPart);
+		when(partRepository.findById(258l)).thenReturn(optionalPart);
+		Optional<Part> optionalHistoryPart = Optional.of(newHistoryPart);
+		when(partRepository.findById(259l)).thenReturn(optionalHistoryPart);
+		
+		componentPersistenceService.updateComponentAndHistory(componentRequest);
+		
+		ArgumentCaptor<Component> componentArg = ArgumentCaptor.forClass(Component.class);
+		verify(componentRepository).save(componentArg.capture());
+		assertThat(componentArg.getValue().getName(), equalTo(componentRequest.getName()));
+		assertThat(componentArg.getValue().getDescription(), equalTo(componentRequest.getDescription()));
+		assertThat(componentArg.getValue().getWorkPerformed(), equalTo(componentRequest.getWorkPerformed()));
+		assertThat(componentArg.getValue().getHoursPerformed(), equalTo(componentRequest.getHoursPerformed()));
+		assertThat(componentArg.getValue().getComponentHistorySet().size(), equalTo(1));
+	}
+
+	@Test
+	public void testUpdateComponentAndHistory_ModifyComponentAndModifyHistoryRecord_Success() throws ApplicationException {
+		// Set up request component
+		ComponentRequest componentRequest = ComponentRequest.builder()
+				.componentUri("http://localhost:6001/components/7").name("OilFilter").description("Champion CH48110")
+				.workPerformed("replaced").datePerformed(new Date()).hoursPerformed(1000f).dateDue(new Date())
+				.hoursDue(1500f).partUri("http://localhost:6001/parts/258").build();
+		// Set up request history record
+		ComponentHistoryVo componentHistoryVo = ComponentHistoryVo.builder()
+				.historyUri("ttp://localhost:6001/componentHistories/24").name("OilFilter")
+				.description("Tempest CH48110").workPerformed("replaced").datePerformed(new Date())
+				.hoursPerformed(1000f).partUri("http://localhost:6001/parts/259").build();
+		componentRequest.getHistoryRequestSet().add(componentHistoryVo);
+		
+		Part oldPart = Part.builder().id(25l).build();
+		Part oldHistoryPart = Part.builder().id(259l).name("part 259 name").build();
+		Part newPart = Part.builder().id(258l).name("part 258 name").build();
+		Part newHistoryPart = Part.builder().id(259l).name("new part 259 name").build();
+		// Create old component with one history record
+		Component oldComponent = Component.builder().id(7l).part(oldPart).name("old name").build();
+		ComponentHistory oldComponentHistory = ComponentHistory.builder().id(24l).name("OilFilter")
+				.description("Tempest CH48110").workPerformed("replaced").datePerformed(new Date())
+				.hoursPerformed(1000f).part(oldHistoryPart).build();
+		oldComponent.getComponentHistorySet().add(oldComponentHistory);
+
+		Optional<Component> optionalComponent = Optional.of(oldComponent);
+		when(componentRepository.findById(7l)).thenReturn(optionalComponent);
+		Optional<Part> optionalPart = Optional.of(newPart);
+		when(partRepository.findById(258l)).thenReturn(optionalPart);
+		Optional<Part> optionalNewHistoryPart = Optional.of(newHistoryPart);
+		when(partRepository.findById(259l)).thenReturn(optionalNewHistoryPart);
+		
+		componentPersistenceService.updateComponentAndHistory(componentRequest);
+		
+		ArgumentCaptor<Component> componentArg = ArgumentCaptor.forClass(Component.class);
+		verify(componentRepository).save(componentArg.capture());
+		assertThat(componentArg.getValue().getName(), equalTo(componentRequest.getName()));
+		assertThat(componentArg.getValue().getDescription(), equalTo(componentRequest.getDescription()));
+		assertThat(componentArg.getValue().getWorkPerformed(), equalTo(componentRequest.getWorkPerformed()));
+		assertThat(componentArg.getValue().getHoursPerformed(), equalTo(componentRequest.getHoursPerformed()));
+		assertThat(componentArg.getValue().getComponentHistorySet().size(), equalTo(1));
+		ComponentHistory newComponentHistory = componentArg.getValue().getComponentHistorySet().iterator().next();
+		assertThat(newComponentHistory.getDescription(), equalTo(componentHistoryVo.getDescription()));
+		assertThat(newComponentHistory.getPart().getId(), equalTo(newHistoryPart.getId()));
+	}
+
+	@Test
+	public void testUpdateComponentAndHistory_ModifyComponentAndDeleteHistoryRecord_Success() throws ApplicationException {
+		// Set up request component with no history record
+		ComponentRequest componentRequest = ComponentRequest.builder()
+				.componentUri("http://localhost:6001/components/7").name("OilFilter").description("Champion CH48110")
+				.workPerformed("replaced").datePerformed(new Date()).hoursPerformed(1000f).dateDue(new Date())
+				.hoursDue(1500f).partUri("http://localhost:6001/parts/258").build();
+		
+		Part oldPart = Part.builder().id(25l).build();
+		Part oldHistoryPart = Part.builder().id(259l).name("part 259 name").build();
+		Part newPart = Part.builder().id(258l).name("part 258 name").build();
+		// Create old component with one history record
+		Component oldComponent = Component.builder().id(7l).part(oldPart).name("old name").build();
+		ComponentHistory oldComponentHistory = ComponentHistory.builder().id(24l).name("OilFilter")
+				.description("Tempest CH48110").workPerformed("replaced").datePerformed(new Date())
+				.hoursPerformed(1000f).part(oldHistoryPart).build();
+		oldComponent.getComponentHistorySet().add(oldComponentHistory);
+
+		Optional<Component> optionalComponent = Optional.of(oldComponent);
+		when(componentRepository.findById(7l)).thenReturn(optionalComponent);
+		Optional<Part> optionalPart = Optional.of(newPart);
+		when(partRepository.findById(258l)).thenReturn(optionalPart);
+		
+		componentPersistenceService.updateComponentAndHistory(componentRequest);
+		
+		ArgumentCaptor<Component> componentArg = ArgumentCaptor.forClass(Component.class);
+		verify(componentRepository).save(componentArg.capture());
+		assertThat(componentArg.getValue().getName(), equalTo(componentRequest.getName()));
+		assertThat(componentArg.getValue().getDescription(), equalTo(componentRequest.getDescription()));
+		assertThat(componentArg.getValue().getWorkPerformed(), equalTo(componentRequest.getWorkPerformed()));
+		assertThat(componentArg.getValue().getHoursPerformed(), equalTo(componentRequest.getHoursPerformed()));
+		assertThat(componentArg.getValue().getComponentHistorySet().size(), equalTo(0));
 	}
 
 }
