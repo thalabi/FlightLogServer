8c13f77d5894820a7d4a20d0cd38908ee3ed7b1e
Tarif Halabi <tarif.halabi@gmail.com>
2020-01-27 13:35:16 -0500

    Add ComponentAndHistoryV entity.

---
+++ FlightLogServer/src/main/java/com/kerneldc/flightlogserver/aircraftmaintenance/domain/componentandhistoryview/ComponentAndHistoryV.java
@@ -0,0 +1,41 @@
+package com.kerneldc.flightlogserver.aircraftmaintenance.domain.componentandhistoryview;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.Immutable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity(name="component_and_history_v")
+@Immutable
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+@Builder
+@ToString
+public class ComponentAndHistoryV {
+
+	@Id
+	private Long id;
+
+    private String componentName;
+    private String componentDescription;
+    private String partName;
+    private String partDescription;
+    private String workPerformed;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date datePerformed;
+    private Float hoursPerformed;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateDue;
+    private Float hoursDue;
+}
