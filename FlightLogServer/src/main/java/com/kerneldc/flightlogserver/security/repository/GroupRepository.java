21b22de6f52f894aa7d0a87559dc41b13d8470f3
Tarif Halabi <tarif.halabi@gmail.com>
2024-10-29 19:44:24 -0400

    Upgrade SpringBoot and use oauth2.

--- FlightLogServer/src/main/java/com/kerneldc/flightlogserver/security/repository/GroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kerneldc.flightlogserver.security.repository;
-
-import java.util.List;
-
-import com.kerneldc.flightlogserver.security.domain.group.Group;
-
-public interface GroupRepository /*extends JpaRepository<Group, Long>, JpaSpecificationExecutor<Group>*/ {
-	List<Group> findAllByOrderByName();
-}
--- FlightLogServer/src/main/java/com/kerneldc/flightlogserver/security/repository/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kerneldc.flightlogserver.security.repository;
-
-import java.util.List;
-
-import com.kerneldc.flightlogserver.security.domain.Permission;
-
-public interface PermissionRepository /*extends JpaRepository<Permission, Long>, JpaSpecificationExecutor<Permission>*/ {
-	List<Permission> findAllByOrderByName();
-}
