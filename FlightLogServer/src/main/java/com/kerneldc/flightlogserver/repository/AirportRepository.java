087cfb95a44264c908490e6f020a1e29a2e8bbb8
Tarif Halabi <tarif.halabi@gmail.com>
2024-12-13 17:37:45 -0500

    Uncomment needed metod.

--- FlightLogServer/src/main/java/com/kerneldc/flightlogserver/repository/AirportRepository.java
+++ FlightLogServer/src/main/java/com/kerneldc/flightlogserver/repository/AirportRepository.java
@@ -1,12 +1,15 @@
 package com.kerneldc.flightlogserver.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.Param;
 
 import com.kerneldc.flightlogserver.domain.airport.Airport;
 
 public interface AirportRepository extends JpaRepository<Airport, Long>, JpaSpecificationExecutor<Airport> {
 //	List<Airport> findAllByOrderByIdentifier();
 //	List<Airport> findAllByOrderByName();
-//	List<Airport> findByIdentifierContainingIgnoreCaseOrNameContainingIgnoreCase(@Param("identifier") String identifier, @Param("name") String name);
+	List<Airport> findByIdentifierContainingIgnoreCaseOrNameContainingIgnoreCase(@Param("identifier") String identifier, @Param("name") String name);
 }
