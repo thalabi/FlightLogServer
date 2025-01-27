adb27906797b79299c8b6ca96d3bddc0ea17dbf1
Tarif Halabi <tarif.halabi@gmail.com>
2025-01-27 18:14:03 -0500

    Refactor.

--- FlightLogServer/src/test/java/com/kerneldc/flightlogserver/exception/RestResponseEntityExceptionHandlerTest.java
+++ FlightLogServer/src/test/java/com/kerneldc/flightlogserver/exception/RestResponseEntityExceptionHandlerTest.java
@@ -1,12 +1,9 @@
 package com.kerneldc.flightlogserver.exception;
 
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.startsWith;
-
-import java.sql.SQLException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,8 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.kerneldc.flightlogserver.exception.RestControllerExceptionHandler.ErrorBody;
-
-import oracle.jdbc.OracleDatabaseException;
 
 class RestResponseEntityExceptionHandlerTest {
 
@@ -60,30 +55,30 @@ class RestResponseEntityExceptionHandlerTest {
 		assertThat(errorBody.stackTrace(), startsWith(ApplicationException.class.getName()));
     }
 	
-	@Test
-	void handleSqlExceptionTest() {
-		String message = "SQL exception message";
-		SQLException sqlException = new SQLException(message);
-		
-		int errorPosition = 7;
-		int oracleErrorNumber = 1403;
-		String oracleErrorMessage = "NO_DATA_FOUND";
-		String sql = "select attribute from table where id = 500;";
-		String originalSql = "exec findId(500)";
-		OracleDatabaseException OracleDatabaseException = new OracleDatabaseException(errorPosition, oracleErrorNumber, oracleErrorMessage, sql, originalSql);
-		
-		sqlException.initCause(OracleDatabaseException);
-		
-		ResponseEntity<ErrorBody> responseEntity = fixture.handleSqlException(sqlException);
-
-		assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.INTERNAL_SERVER_ERROR));
-		assertThat(responseEntity.getBody(), instanceOf(ErrorBody.class));
-		ErrorBody exceptionBean = responseEntity.getBody();
-		assertThat(exceptionBean.message(), containsString(message));
-		assertThat(exceptionBean.message(), containsString(oracleErrorNumber+""));
-		assertThat(exceptionBean.stackTrace(), startsWith(SQLException.class.getName()));
-		assertThat(exceptionBean.message(), containsString(oracleErrorMessage));
-		assertThat(exceptionBean.message(), containsString(sql));
-		assertThat(exceptionBean.message(), containsString(originalSql));
-	}
+//	@Test
+//	void handleSqlExceptionTest() {
+//		String message = "SQL exception message";
+//		SQLException sqlException = new SQLException(message);
+//		
+//		int errorPosition = 7;
+//		int oracleErrorNumber = 1403;
+//		String oracleErrorMessage = "NO_DATA_FOUND";
+//		String sql = "select attribute from table where id = 500;";
+//		String originalSql = "exec findId(500)";
+//		OracleDatabaseException OracleDatabaseException = new OracleDatabaseException(errorPosition, oracleErrorNumber, oracleErrorMessage, sql, originalSql);
+//		
+//		sqlException.initCause(OracleDatabaseException);
+//		
+//		ResponseEntity<ErrorBody> responseEntity = fixture.handleSqlException(sqlException);
+//
+//		assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.INTERNAL_SERVER_ERROR));
+//		assertThat(responseEntity.getBody(), instanceOf(ErrorBody.class));
+//		ErrorBody exceptionBean = responseEntity.getBody();
+//		assertThat(exceptionBean.message(), containsString(message));
+//		assertThat(exceptionBean.message(), containsString(oracleErrorNumber+""));
+//		assertThat(exceptionBean.stackTrace(), startsWith(SQLException.class.getName()));
+//		assertThat(exceptionBean.message(), containsString(oracleErrorMessage));
+//		assertThat(exceptionBean.message(), containsString(sql));
+//		assertThat(exceptionBean.message(), containsString(originalSql));
+//	}
 }
