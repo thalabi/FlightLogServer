86d2c9f45918bdbc25447fce3395779e3fd9ca59
Tarif Halabi <tarif.halabi@gmail.com>
2025-01-27 18:11:39 -0500

    Disable batch configuration.

--- FlightLogServer/src/main/java/com/kerneldc/flightlogserver/springBootConfig/MapBasedJobRepositoryConfig.java
+++ FlightLogServer/src/main/java/com/kerneldc/flightlogserver/springBootConfig/MapBasedJobRepositoryConfig.java
@@ -2,15 +2,11 @@ package com.kerneldc.flightlogserver.springBootConfig;
 
 import javax.sql.DataSource;
 
-import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.context.annotation.Configuration;
+//@Configuration
+//@EnableBatchProcessing
+public class MapBasedJobRepositoryConfig /*extends DefaultBatchConfigurer*/ {
 
-@Configuration
-@EnableBatchProcessing
-public class MapBasedJobRepositoryConfig extends DefaultBatchConfigurer {
-
-    @Override
+    //@Override
     public void setDataSource(DataSource dataSource) {
         // override to do not set datasource even if a datasource exist.
         // initialize will use a Map based JobRepository (instead of database)
